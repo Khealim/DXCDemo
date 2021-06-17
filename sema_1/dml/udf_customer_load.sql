8b2d9d3444f92e29d8fb8f9a624e003f209da100
tamas.katona <tamas.katona@dxc.com>
2021-06-17 02:30:04 +0200

    new argument and insert list with new values

--- sema_1/dml/udf_customer_load.sql
+++ sema_1/dml/udf_customer_load.sql
@@ -1,6 +1,10 @@
-create or replace function sema_1.customer_load(in param1 text, in param2 text)
+create or replace function sema_1.customer_load(in param1 text, in param2 text, in param3 text)
 LANGUAGE SQL
 AS $$
 INSERT INTO sema_1.customer VALUES (param1 + param2);
 INSERT INTO sema_1.customer VALUES (param1 - param2);
+INSERT INTO sema_1.customer VALUES (param3 + param2);
+INSERT INTO sema_1.customer VALUES (param3 - param2);
+INSERT INTO sema_1.customer VALUES (param1 + param3);
+INSERT INTO sema_1.customer VALUES (param1 - param3);
 $$;
